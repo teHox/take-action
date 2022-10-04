cd3215eb64cf0524b178010ca64b04194ed42066
teHox <61248895+teHox@users.noreply.github.com>
2022-10-04 21:56:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,6 +11,27 @@ function domLoaded() {
     opacity.classList.toggle("opacity_active");
   });
 
+  function popup() {
+    const popUpShowButton = document.querySelector("#button-consultation");
+    const popUp = document.querySelector("#consultation");
+    const closeButton = document.querySelector("#close-consultation");
+  
+    function closePopUp() {
+      popUp.style.top = "-300%";
+    }
+  
+    popUpShowButton.addEventListener("click", () => {
+      popUp.style.top = "0";
+    });
+  
+    popUp.addEventListener("click", function (e) {
+      if (e.target == this) closePopUp();
+    });
+    closeButton.addEventListener("click", closePopUp);
+  }
+  window.addEventListener("load", popup);
+  
+
   const playButton = document.querySelector(".about-us__play");
   const video = document.querySelector("#myVideo");
   playButton.addEventListener("click", () => {
